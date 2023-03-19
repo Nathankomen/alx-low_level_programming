a539cbbff4e8f5e6265c2f36fdee10a885c1a297
Nathankomen <nathanyato2001@gmail.com>
2023-03-18 23:53:12 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+putchar('\n');
+return (0);
+}
