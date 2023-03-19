db7a96a3f13ea77c51dd459c377b37f89ca35e1e
Nathankomen <nathanyato2001@gmail.com>
2023-03-18 22:39:15 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,21 +7,22 @@
 
 int main(void)
 {
-	char c;
+char c;
 
-	char d;
+char d;
 
-	c = 'a';
-	d = 'A';
-	while
-		(c <= 'z') {
-			putchar(c);
-			c++;
-			}
-	while
-		(d <= 'z') {
-			putchar(d);
-			d++;
-			}
-       	return (0);
+c = 'a';
+d = 'A';
+while
+	(c <= 'z') {
+		putchar(c);
+		c++;
+		}
+while
+	(d <= 'z') {
+		putchar(d);
+		d++;
+		}
+putchar('\n');
+return (0);
 }
