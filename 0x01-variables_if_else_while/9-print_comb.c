39bf1955b13f2996ccf0ffac3c2c0b442f62d49a
Nathankomen <nathanyato2001@gmail.com>
2023-03-19 02:23:32 -0700

    all possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int a;
+for (a = 0; a < 10; a++)
+{
+putchar(a + '0');
+if (a < 9)
+{
+	putchar(',');
+	putchar(' ');
+	}
+}
+putchar('\n');
+return (0);
+}
