786db5433bdd2da4e7d63f255a271294536591c8
Nathankomen <nathanyato2001@gmail.com>
2023-03-19 03:45:54 -0700

    Inventing is a combination of brains and materials

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int i;
+int j;
+
+for (i = 0; i < 10; i++)
+{
+for (j = 1; j < 10; j++)
+{
+if (i < j && i != j)
+{
+putchar(i + '0');
+putchar(j + '0');
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
