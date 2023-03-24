ff6ed26e3a0c9e00803a0457327e2bf9b9551525
Nathankomen <nathanyato2001@gmail.com>
2023-03-24 12:22:29 -0700

    Fizz-Buzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,13 +2,12 @@
 #include <stdio.h>
 /**
  * main - entry point
- * @size: size of the square
  * description: print numbers 1-100 w/
  * Fizz for multiple 3, Buzz for multiples of 5.
- * FizzBuzz for multiples of both 
- * Return: always 0
+ * FizzBuzz for multiples of both
+ *
+ * Return:always 0
  */
-
 int main(void)
 {
 int i;
