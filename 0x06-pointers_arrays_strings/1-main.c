13c069457470f523b87e8e984d984609ee3631fe
“Nathankomen” <nathanyato2001@gmail.com>
2023-03-30 02:20:35 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-main.c
+++ 0x06-pointers_arrays_strings/1-main.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
+char *_strncat(char *dest, char *src, int n);
 int main(void)
 {
 char s1[98] = "Hello ";
@@ -22,5 +22,5 @@ ptr = _strncat(s1, s2, 1024);
 printf("%s", s1);
 printf("%s", s2);
 printf("%s", ptr);
-eturn (0);
+return (0);
 }
