e72885cb5c591f61a954cc1b92aee8de12d14f3b
“Nathankomen” <nathanyato2001@gmail.com>
2023-04-06 00:46:14 -0700

    natural square root of a number.

--- 0x08-recursion/5-main.c
+++ 0x08-recursion/5-main.c
@@ -6,6 +6,7 @@
  *
  * Return: Always 0.
  */
+int _sqrt_recursion(int n);
 int main(void)
 {
 int r;
---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _sqrt_recursion - print sqrt of value
+* @n: number to calculate sqrt
+* Return: square root
+*/
+int _sqrt(int n, int x);
+int _sqrt_recursion(int n)
+{
+return (_sqrt(n, 1));
+}
+/**
+* _sqrt - calculate sqrt
+* @n: number to calculate sqrt
+* @x: number to iterate
+* Return: square root
+*/
+int _sqrt(int n, int x)
+{
+int sqrt = x * x;
+if (sqrt > n)
+return (-1);
+if (sqrt == n)
+return (x);
+return (_sqrt(n, x + 1));
+}
