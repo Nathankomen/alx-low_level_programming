cfa5c394e54f29b461b6840244b921c2d95ef090
“Nathankomen” <nathanyato2001@gmail.com>
2023-04-11 02:14:43 -0700

    function that creates an array of chars, and initializes it with a specific cha

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup -  returns a pointer to a newly allocated space in memory
+ * contains a copy of the string given as a parameter.
+ * @str: array size
+ * Return: array
+ */
+char *_strdup(char *str)
+{
+int d = 0, e = 1;
+char *s;
+if (str == NULL)
+return (NULL);
+while (str[e])
+{
+e++;
+}
+s = malloc((sizeof(char) * e) + 1);
+if (s == NULL)
+return (NULL);
+while (d < e)
+{
+s[d] = str[d];
+d++;
+}
+s[d] = '\0';
+return (s);
+}
