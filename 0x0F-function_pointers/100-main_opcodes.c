e19370798fe390efb9ebd985aa1e9b3d68b5e74c
“Nathankomen” <nathanyato2001@gmail.com>
2023-04-20 10:14:58 -0700

    program that performs simple operations.

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print own opcodes
+ * @argc: number of argument
+ * @argv: arguments array
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+int byte, j;
+char *arr;
+if (argc != 2)
+{
+printf("Error\n");
+exit(1);
+}
+bytes = atoi(argv[1]);
+if (byte < 0)
+{
+printf("Error\n");
+exit(2);
+}
+arr = (char *)main;
+for (j = 0;  < byte; j++)
+{
+if (j == byte - 1)
+{
+printf("%02hhx\n", arr[j]);
+break;
+}
+printf("%02hhx ", arr[j]);
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+/**
+ * main - Prints the result of operation.
+ * @argc: The number of arguments
+ * @argv: An array of pointers
+ * Return: Always 0.
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+int num, num1;
+char *op;
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+num = atoi(argv[1]);
+op = argv[2];
+num1 = atoi(argv[3]);
+if (get_op_func(op) == NULL || op[1] != '\0')
+{
+printf("Error\n");
+exit(99);
+}
+if ((*op == '/' && num1 == 0) ||
+(*op == '%' && num1 == 0))
+{
+printf("Error\n");
+exit(100);
+}
+printf("%d\n", get_op_func(op)(num, num1));
+return (0);
+}
